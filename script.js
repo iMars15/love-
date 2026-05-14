271c65e34bb3f5de3433b9e2ef3d0a616cf3cf9d
iMars-dev <agishevmars@yandex.ru>
2026-05-14 23:49:45 +0300

    fix

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const columns = Math.floor(canvas.width / fontSize);
 const drops = Array.from({length: columns}, () => Math.floor(Math.random() * -100));
 
 let heartScale = 0;
-let targetHeartScale = 18; 
+const targetHeartScale = 18; 
 let hue = 0;
 let isActivated = false;
 
@@ -26,6 +26,8 @@ function inHeart(x, y, centerX, centerY, scale) {
 }
 
 function drawMatrix() {
+    requestAnimationFrame(drawMatrix);
+
     ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -34,11 +36,10 @@ function drawMatrix() {
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2 - 40;
 
-    if (isActivated && heartScale < targetHeartScale) {
-        heartScale += 0.12;
-    }
-
     if (isActivated) {
+        if (heartScale < targetHeartScale) {
+            heartScale += 0.12;
+        }
         hue = (hue + 1.5) % 360;
     }
 
@@ -53,7 +54,7 @@ function drawMatrix() {
             if (inside) {
                 ctx.fillStyle = `hsl(${hue}, 100%, 60%)`;
                 ctx.shadowColor = `hsl(${hue}, 100%, 50%)`;
-                ctx.shadowBlur = 8;
+                ctx.shadowBlur = 4;
             } else {
                 ctx.fillStyle = '#2d0000';
                 ctx.shadowBlur = 0;
@@ -70,92 +71,26 @@ function drawMatrix() {
     }
 }
 
-setInterval(drawMatrix, 22);
+requestAnimationFrame(drawMatrix);
 
 const phrases = [
-    "我爱你",
-    "Te amo",
-    "Je t'aime",
-    "Ich liebe dich",
-    "Ti amo",
-    "愛してる",
-    "사랑해",
-    "Eu te amo",
-    "أنا أحبك",
-    "मैं तुमसे प्यार करता हूँ",
-    "Main tumse pyar karta hoon",
-    "Seni seviyorum",
-    "Ek is lief vir jou",
-    "Unë të dua",
-    "Я цябе кахаю",
-    "Я тебя люблю",
-    "I love you",
-    "Ես քեզ սիրում եմ",
-    "আমি তোমায় ভালোবাসি",
-    "Volim te",
-    "Обичам те",
-    "Estimo molt",
-    "Gihigugma tika",
-    "Ndimakukonda",
-    "Tá grá agam duit",
-    "Volim te",
-    "Miluji tě",
-    "Jeg elsker dig",
-    "Ik hou van jou",
-    "Mi amas vin",
-    "Ma armastan sind",
-    "Jag älskar dig",
-    "Mahal kita",
-    "Minä rakastan sinua",
-    "S'agapo",
-    "Je t'aimé",
-    "אני אוהב אותך",
-    "मैं तुझसे प्रेम करतो",
-    "Kuv hlub koj",
-    "Szeretlek",
-    "Ég elska þig",
-    "Aku cinta kamu",
-    "Ṇdị m hụrụ gị n'anya",
-    "Gràdhaim thu",
-    "Ti voglio bene",
-    "愛你",
-    "Ес сени суюм",
-    "Nakupenda",
-    "Kocham cię",
-    "Eu te iubesc",
-    "Я тебе люблю",
-    "Volim te",
-    "Milujem ťa",
-    "Rad te imam",
-    "Waan ku jeclahay",
-    "Ngiyakuthanda",
-    "Te quiero",
-    "Aku sayang engkau",
-    "നിന്നെ ഞാൻ പ്രേമിക്കുന്നു",
-    "Jeg elsker deg",
-    "Ek het jou lief",
-    "Мен сени сүйөм",
-    "Би тэмд хайртай",
-    "म तिमीलाई माया गर्छु",
-    "دوستت دارم",
-    "Kocham cie",
-    "Aroha ahau ki a koe",
-    "Ke a go rata",
-    "Ndinokuda",
-    "Mahal kita",
-    "நான் உன்னை காதலிக்கிறேன்",
-    "నేను నిన్ను ప్రేమిస్తున్నాను",
-    "ฉันรักคุณ",
-    "Seni seviyorum",
-    "Я люблю тебе",
-    "Men seni söýýärin",
-    "Я люблю тебе",
-    "Men seni sevaman",
-    "Anh yêu em",
-    "Rwy'n dy garu di",
-    "Ich han dich gärn",
-    "Ik hald fan dy",
-    "Ndinokutenda",
+    "我爱你", "Te amo", "Je t'aime", "Ich liebe dich", "Ti amo", "愛してる", "사랑해", 
+    "Eu te amo", "أنا أحبك", "मैं तुमसे प्यार करता हूँ", "Main tumse pyar karta hoon", 
+    "Seni seviyorum", "Ek is lief vir jou", "Unë të dua", "Я цябе кахаю", "Я тебя люблю", 
+    "I love you", "Ես քеզ սիրում եմ", "আমি তোমায় ভালোবাসি", "Volim te", "Обичам те", 
+    "Estimo molt", "Gihigugma tika", "Ndimakukonda", "Tá grá agam duit", "Volim te", 
+    "Miluji tě", "Jeg elsker dig", "Ik hou van jou", "Mi amas vin", "Ma armastan sind", 
+    "Jag älskar dig", "Mahal kita", "Minä rakastan sinua", "S'agapo", "Je t'aimé", 
+    "אני אוهב אותך", "मैं तुझसे प्रेम करतो", "Kuv hlub koj", "Szeretlek", "Ég elska þig", 
+    "Aku cinta kamu", "Ṇdị m hụrụ gị n'anya", "Gràdhaim thu", "Ti voglio bene", "愛你", 
+    "Ес сени суюм", "Nakupenda", "Kocham cię", "Eu te iubesc", "Я тебе люблю", "Volim te", 
+    "Milujem ťa", "Rad te imam", "Waan ku jeclahay", "Ngiyakuthanda", "Te quiero", 
+    "Aku sayang engkau", "നിന്നെ ഞാൻ പ്രേമിക്കുന്നു", "Jeg elsker deg", "Ek het jou lief", 
+    "Мен сени сүйөм", "Би тэмд хайртай", "म तिमीलाई माया गर्छु", "دوستت دارم", "Kocham cie", 
+    "Aroha ahau ki a koe", "Ke a go rata", "Ndinokuda", "Mahal kita", "நான் உன்னை காதலிக்கிறேன்", 
+    "నేను నిన్ను ప్రేమిస్తున్నాను", "ฉันรักคุณ", "Seni seviyorum", "Я люблю тебе", 
+    "Men seni söýýärin", "Я люблю тебе", "Men seni sevaman", "Anh yêu em", "Rwy'n dy garu di", 
+    "Ich han dich gärn", "Ik hald fan dy", "Ndinokutenda"
 ];
 
 const btn = document.getElementById('startBtn');
